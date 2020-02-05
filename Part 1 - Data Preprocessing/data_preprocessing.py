4b9146c4653c278603e92f2900a570b3eebfbd55
jfriboldi <jfriboldi@gmail.com>
2020-02-05 13:43:03 -0800

    simple linear regression

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#import the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:,3].values
+
 
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
@@ -20,11 +22,12 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
+#splitting the dataset into the training set and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 #Feature Scaling 
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+X_test = sc_X.transform(X_test)"""
